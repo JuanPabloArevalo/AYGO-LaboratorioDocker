6ec862c00f1c0ae2be521b8d4a5a13e7709faa19
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2020-10-03 00:34:15 -0500

    Cambio 1

--- src/main/java/edu/eci/aygo/restcontrollers/CadenaResourceController.java
+++ src/main/java/edu/eci/aygo/restcontrollers/CadenaResourceController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +26,7 @@ public class CadenaResourceController {
     @Autowired
     Services services;
     
+    @CrossOrigin(origins = "*")
     @RequestMapping(path = "/actuales", method = RequestMethod.POST, consumes =MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addCadena(@RequestBody Cadena cadena) {
         try {
